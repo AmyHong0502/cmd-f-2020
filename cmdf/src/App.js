340cb4ad917b5520ab85c2cbfeb2673a597a5b88
Amy Hong <git@amyhong.dev>
2020-03-07 15:05:15 -0800

    extract AppBar

--- cmdf/src/App.js
+++ cmdf/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import AppBar from "@material-ui/core/AppBar";
+
 import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+
 import Footer from "./components/footer/footer";
+import MainAppBar from './components/mainappbar/mainappbar';
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -55,14 +55,7 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon className={classes.icon} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Album layout
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <MainAppBar />
       <main>
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
