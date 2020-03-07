a151335ed6e5583dec35bad7958dc9a3eb0e1022
Amy Hong <git@amyhong.dev>
2020-03-07 15:31:15 -0800

    extract navigation

--- cmdf/src/App.js
+++ cmdf/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
+import Navigation from "./components/navigation/navigation";
 import Main from "./components/main/main";
 import MainAppBar from "./components/mainappbar/mainappbar";
 import Footer from "./components/footer/footer";
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <CssBaseline />
+      <Navigation />
       <MainAppBar />
       <Main />
       <Footer />
--- cmdf/src/components/mainappbar/mainappbar.jsx
+++ cmdf/src/components/mainappbar/mainappbar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
+
 import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
+
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -15,9 +14,6 @@ const useStyles = makeStyles(theme => ({
   },
   heroButtons: {
     marginTop: theme.spacing(4)
-  },
-  icon: {
-    marginRight: theme.spacing(2)
   }
 }));
 
@@ -26,14 +22,6 @@ export default function MainAppBar() {
 
   return (
     <>
-      <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon className={classes.icon} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Album layout
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <div className={classes.heroContent}>
         <Container maxWidth="sm">
           <Typography
