a00e808870ab16217d9ab6e18bc447bf0c3ef811
Amy Hong <git@amyhong.dev>
2020-03-07 15:31:53 -0800

    Create siteConfig.js

---
+++ cmdf/src/siteConfig.js
@@ -0,0 +1,24 @@
+const siteConfig = {
+  title: "Hero",
+  url: "https://TODO_hero.com",
+  baseUrl: "/",
+
+  headerIcon: "images/favicon.ico",
+  footerIcon: "images/favicon.ico",
+  favicon: "images/favicon.ico",
+
+  colors: {
+    primary: "#111",
+    secondary: "#FA0"
+  },
+
+  fonts: {
+    serif: ["Times New Roman", "Serif"],
+    sansSerif: ["Roboto", "helvetica"],
+    display: [],
+    monoSpace: []
+  },
+  copyright: `Copyright © ${new Date().getFullYear()} HERO TODO`
+};
+
+module.exports = siteConfig;
