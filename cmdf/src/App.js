5725311d0885f9102d6796702dbdee7a93a53686
Amy Hong <git@amyhong.dev>
2020-03-07 14:58:37 -0800

    extract Footer

--- cmdf/src/App.js
+++ cmdf/src/App.js
@@ -13,20 +13,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import Footer from "./components/footer/footer";
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -147,20 +134,7 @@ function Album() {
           </Grid>
         </Container>
       </main>
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
+      <Footer />
     </>
   );
 }
