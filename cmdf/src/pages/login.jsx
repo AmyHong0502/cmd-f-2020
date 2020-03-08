76069ea0b0ee63901ead0bf4b6f4817ee3b7e0cd
Amy Hong <git@amyhong.dev>
2020-03-07 22:15:59 -0800

    update login UI

--- cmdf/src/pages/login.jsx
+++ cmdf/src/pages/login.jsx
@@ -1,18 +1,92 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-class Login extends React.Component {
-  render() {
-    const { handleSignIn } = this.props;
-
-    return (
-      <div>
-        Login
-        <button onClick={ handleSignIn.bind(this) }>
-          Sign in with Blockstack
-        </button>
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100vh"
+  },
+  image: {
+    backgroundImage: "url(./game_background_4.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "dark"
+        ? theme.palette.grey[900]
+        : theme.palette.grey[50],
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
   }
-}
+}));
 
-export default Login;
+export default function Login(props) {
+  const classes = useStyles();
+  const { handleSignIn } = props;
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Finance Hero
+          </Typography>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSignIn.bind(this)}
+          >
+            Sign In
+          </Button>
+          <Grid container justify="center">
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+          <Box mt={5}>
+            <Typography variant="body2" color="textSecondary" align="center">
+              Happy Gaming!
+              <br />
+              Crafted with ❤️ at cmd-f 2020
+            </Typography>
+          </Box>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
