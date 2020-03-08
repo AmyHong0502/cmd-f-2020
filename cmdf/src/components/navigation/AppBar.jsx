9205a791a21075155c3b395766f2781fa5947c6d
alice.kim <alice.kim@concur.com>
2020-03-07 20:06:39 -0800

    Create generic dialog

--- cmdf/src/components/navigation/AppBar.jsx
+++ cmdf/src/components/navigation/AppBar.jsx
@@ -7,11 +7,9 @@ import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Drawer from '@material-ui/core/Drawer';
-import Fab from '@material-ui/core/Fab';
 import List from '@material-ui/core/List';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -52,11 +50,6 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     justifyContent: 'space-between',
     padding: theme.spacing(8),
-  },
-  fab: {
-    position: 'fixed',
-    right: 30,
-    bottom: 30,
   },
   title: {
     flexGrow: 1,
@@ -162,16 +155,16 @@ export default function MainAppBar() {
             Dashboard
           </Typography>
           <IconButton>
-            <FavoriteRoundedIcon style={{ color: 'orange' }} />
+            <Icon className="fas fa-coins" style={{ color: 'orange' }} />
           </IconButton>
           <Typography variant="h6" style={{ marginRight: 12 }} noWrap>
             250
           </Typography>
           <IconButton>
-            <RedeemRoundedIcon style={{ color: 'orange' }} />
+            <Icon className="fas fa-wallet" style={{ color: 'orange' }} />
           </IconButton>
           <Typography variant="h6" noWrap>
-            200.00
+            $200.00
           </Typography>
           <Button style={{ color: 'orange', fontSize: '1.20rem', marginLeft: 10 }}>Sign Out</Button>
         </Toolbar>
@@ -218,9 +211,6 @@ export default function MainAppBar() {
           </div>
           <ProgressChart />
         </div>
-        <Fab className={classes.fab} color="primary" aria-label="add">
-          <AddIcon />
-        </Fab>
       </div>
     </div>
   );
