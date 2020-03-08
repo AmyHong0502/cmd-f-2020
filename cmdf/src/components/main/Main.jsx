958940ff1f5977a34b3582776950cbf1f61492f7
Alice <alice828@mail.ubc.ca>
2020-03-07 18:16:57 -0800

    Create Imcome, Expense, and Tasks Columns (#4)
    * Project restructure, main page work
    
    * Create custom progress bar
    
    * Optimize import statements
    
    * Add number next to progress bar
    
    * Add progress graph
    
    * Create income, expense, tasks columns
    
    Co-authored-by: alice.kim <alice.kim@concur.com>

--- cmdf/src/components/main/Main.jsx
+++ cmdf/src/components/main/Main.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
-  }
+    padding: theme.spacing(8),
+  },
+  paper: {
+    height: 140,
+  },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = ['Income', 'Expense', 'Tasks'];
 
 export default function Main() {
   const classes = useStyles();
 
   return (
     <main>
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
-          {cards.map(card => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
-              Lorem ipsum dolor sit amet
+      <Paper className={classes.cardGrid}>
+        <Grid container justify="center" spacing={8}>
+          {cards.map((card, i) => (
+            <Grid item key={i} xs={12} sm={6} md={4}>
+              <Typography variant="h6">{card}</Typography>
+              <Paper className={classes.paper}></Paper>
             </Grid>
           ))}
         </Grid>
-      </Container>
+      </Paper>
     </main>
   );
 }
