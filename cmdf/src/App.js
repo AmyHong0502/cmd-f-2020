d0c88d887f37e2c918b5b335d50a7cac266ace0e
Alice <alice828@mail.ubc.ca>
2020-03-07 16:54:43 -0800

    Create Main Page (#1)
    * Project restructure, main page work
    
    * Create custom progress bar
    
    * Optimize import statements
    
    Co-authored-by: alice.kim <alice.kim@concur.com>

--- cmdf/src/App.js
+++ cmdf/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import Navigation from "./components/navigation/navigation";
-import Main from "./components/main/main";
-import MainAppBar from "./components/mainappbar/mainappbar";
-import Footer from "./components/footer/footer";
+import Navigation from "./components/navigation/Navigation";
+import Main from "./components/main/Main";
+import MainAppBar from "./components/navigation/AppBar";
+import Footer from "./components/footer/Footer";
 
 import Register from "./pages/register";
 import Login from "./pages/login";
