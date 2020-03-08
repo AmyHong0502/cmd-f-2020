65be255d4ef30fb926ffd485c121c317d830c29a
Alice <alice828@mail.ubc.ca>
2020-03-08 10:55:43 -0700

    Add GIFs (#11)
    * Project restructure, main page work
    
    * Create custom progress bar
    
    * Optimize import statements
    
    * Add number next to progress bar
    
    * Add progress graph
    
    * Create income, expense, tasks columns
    
    * Create generic dialog
    
    * Allow switching tabs
    
    * Fill main content for each tab with stubs
    
    * Adjust CSS
    
    * Add Pokemon GIFs
    
    * Render Pokemon GIFs as assets
    
    * Add background color to different categories in history table
    
    Co-authored-by: alice.kim <alice.kim@concur.com>

--- cmdf/src/components/main/assets/AssetsGrid.jsx
+++ cmdf/src/components/main/assets/AssetsGrid.jsx
@@ -2,23 +2,46 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 import Paper from '@material-ui/core/Paper';
+import dratini1 from '../../../assets/Dratini1.gif';
+import dratini2 from '../../../assets/Dratini2.gif';
+import dratini3 from '../../../assets/Dratini3.gif';
+import pikachu1 from '../../../assets/Pikachu1.gif';
+import pikachu2 from '../../../assets/Pikachu2.gif';
+import pikachu3 from '../../../assets/Pikachu3.gif';
+import squirtle1 from '../../../assets/Squirtle1.gif';
+import squirtle2 from '../../../assets/Squirtle2.gif';
+import squirtle3 from '../../../assets/Squirtle3.gif';
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
         flexWrap: 'wrap',
-        justifyContent: 'space-around',
+        justifyContent: 'flex-start',
     },
 }));
+
+const gifs = [
+    dratini1, dratini2, dratini3,
+    pikachu1, pikachu2, pikachu3,
+    squirtle1, squirtle2, squirtle3,
+];
 
 export default function Assets() {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            {Array(12).fill(0).map((item, i) =>
-                <Paper key={i} style={{ padding: 50, margin: 50 }} elevation={4}>
-                    <Icon className="fas fa-coins"></Icon>
+            {Array(9).fill(0).map((item, i) =>
+                <Paper key={i} style={{
+                    width: 200,
+                    height: 200,
+                    margin: 50,
+                    display: 'flex',
+                }} elevation={4}>
+                    <img src={gifs[i]} style={{
+                        margin: 'auto',
+                        alignSelf: 'center'
+                    }} />
                 </Paper>
             )}
         </div >
