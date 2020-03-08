1fd361960e249752c71e7bd3ed307ba7ec41afe4
Amy Hong <git@amyhong.dev>
2020-03-07 23:56:59 -0800

    update logo

--- cmdf/src/pages/login.jsx
+++ cmdf/src/pages/login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,11 +10,12 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { Redirect } from "react-router-dom";
-
 const useStyles = makeStyles(theme => ({
   root: {
     height: "100vh"
+  },
+  logo: {
+    width: "45%"
   },
   image: {
     backgroundImage: "url(./game_background_4.png)",
@@ -30,10 +32,6 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: "100%", // Fix IE 11 issue.
@@ -58,12 +56,7 @@ export default function Login(props) {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Finance Hero
-          </Typography>
+          <img src="./logo_transparent.png" className={classes.logo} />
           <Button
             type="submit"
             fullWidth
